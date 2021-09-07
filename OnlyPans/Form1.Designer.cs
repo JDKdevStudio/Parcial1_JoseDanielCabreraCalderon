198c55fd049c300b858152e0b48951bbb0c633a3
Jose Daniel Cabrera Calderon <jose.cabrera.2021@upb.edu.co>
2021-09-07 11:11:19 -0500

    Inicio de sesión y control de sus validaciones terminado

--- OnlyPans/Form1.Designer.cs
+++ OnlyPans/Form1.Designer.cs
@@ -29,13 +29,131 @@ namespace OnlyPans
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passInput = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoginImage = new System.Windows.Forms.PictureBox();
+            this.Login = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // userInput
+            // 
+            this.userInput.Location = new System.Drawing.Point(307, 257);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(150, 20);
+            this.userInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(336, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(321, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contraseña";
+            // 
+            // passInput
+            // 
+            this.passInput.Location = new System.Drawing.Point(307, 357);
+            this.passInput.Name = "passInput";
+            this.passInput.Size = new System.Drawing.Size(150, 20);
+            this.passInput.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::OnlyPans.Properties.Resources.empleadof;
+            this.pictureBox2.Location = new System.Drawing.Point(588, 238);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::OnlyPans.Properties.Resources.adminf;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginImage
+            // 
+            this.LoginImage.Image = global::OnlyPans.Properties.Resources.panadero;
+            this.LoginImage.Location = new System.Drawing.Point(307, 49);
+            this.LoginImage.Name = "LoginImage";
+            this.LoginImage.Size = new System.Drawing.Size(150, 150);
+            this.LoginImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LoginImage.TabIndex = 0;
+            this.LoginImage.TabStop = false;
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(307, 408);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(150, 23);
+            this.Login.TabIndex = 7;
+            this.Login.Text = "Ingresar";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.passInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userInput);
+            this.Controls.Add(this.LoginImage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Iniciar Sesion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox LoginImage;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passInput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button Login;
     }
 }
 
--- OnlyPans/Form1.cs
+++ OnlyPans/Form1.cs
@@ -16,5 +16,34 @@ namespace OnlyPans
         {
             InitializeComponent();
         }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            if (userInput.Text == "admin" && passInput.Text == "admin")
+            {
+                using (MainPanel mainPanel = new MainPanel()) {
+                    mainPanel.UserType = "admin";
+                    this.Hide();
+                    mainPanel.ShowDialog();
+                    this.Close();
+                }
+            }
+            else if (userInput.Text == "panadero" && passInput.Text == "panadero")
+            {
+                using (MainPanel mainPanel = new MainPanel())
+                {
+                    mainPanel.UserType = "panadero";
+                    this.Hide();
+                    mainPanel.ShowDialog();
+                    this.Close();
+                }
+            }
+            else
+            {
+                userInput.Clear();
+                passInput.Clear();
+                MessageBox.Show("Usuario incorrecto/no existe");
+            }
+        }
     }
 }
